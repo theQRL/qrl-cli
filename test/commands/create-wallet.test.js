8368f8e112507b2f5f7ab506106f32145ab6405c
JP Lomas <jp.lomas@gmail.com>
2019-07-21 21:25:21 +0100

    More create-wallet unit tests

--- test/commands/create-wallet.test.js
+++ test/commands/create-wallet.test.js
@@ -79,6 +79,27 @@ describe('create-wallet', () => {
 describe('create-wallet', () => {
   let args = [
     'create-wallet',
+    '-f',
+    '/tmp/wallet.json',
+    '-p',
+    'joan clarke was here',
+  ]
+  let exitCode
+  before(done => {
+    let process = spawn('./bin/run', args)
+    process.on('exit', code => {
+      exitCode = code
+      done()
+    })
+  })
+  it('exit code should be 0 if passed with -p flag and a valid wallet file password', () => {
+    assert.strictEqual(exitCode, 0)
+  })
+})
+
+describe('create-wallet', () => {
+  let args = [
+    'create-wallet',
     '-h',
   ]
   let exitCode
@@ -221,5 +242,24 @@ describe('create-wallet', () => {
   })
   it('exit code should be 0 if passed with valid hash selection flag', () => {
     assert.strictEqual(exitCode, 0)
+  })
+})
+
+describe('create-wallet', () => {
+  let args = [
+    'create-wallet',
+    '-3',
+    '-2',
+  ]
+  let exitCode
+  before(done => {
+    let process = spawn('./bin/run', args)
+    process.on('exit', code => {
+      exitCode = code
+      done()
+    })
+  })
+  it('exit code should be non-0 if passed with multiple hash selection flags', () => {
+    assert.notStrictEqual(exitCode, 0)
   })
 })
