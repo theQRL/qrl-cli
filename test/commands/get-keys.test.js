4b202058e5b21f00ec20ed4bda3a5f998613b9a3
fr1t2 <fr1t2@protonmail.com>
2021-01-24 10:58:59 -0700

    print pub_lattice_keys to file or output json tests

--- test/commands/get-keys.test.js
+++ test/commands/get-keys.test.js
@@ -101,7 +101,7 @@ describe('get-keys #5', () => {
     '-p',
     '1',
     '-g',
-    'notAnAddress',
+    'invalid.theqrl.org:19009',
   ]
   let exitCode
   before(done => {
@@ -116,7 +116,7 @@ describe('get-keys #5', () => {
   })
 })
 
-// get keys from address given
+// get keys from address given testnet
 describe('get-keys #6', () => {
   const args = [
     'get-keys',
@@ -136,7 +136,153 @@ describe('get-keys #6', () => {
       done()
     })
   })
-  it('exit code should be 0 if everything is correct', () => {
+  it('exit code should be 0 if everything is correct with keys printed to console for testnet', () => {
     assert.strictEqual(exitCode, 0)
   })
 })
+
+// get keys from address given mainnet
+describe('get-keys #7', () => {
+  const args = [
+    'get-keys',
+    '-a',
+    'Q020200cf30b98939844cecbaa20e47d16b83aa8de58581ec0fda34d83a42a5a665b49986c4b832',
+    '-i',
+    '1',
+    '-p',
+    '1',
+    '-m',
+  ]
+  let exitCode
+  before(done => {
+    const process = spawn('./bin/run', args, processFlags)
+    process.on('exit', code => {
+      exitCode = code
+      done()
+    })
+  })
+  it('exit code should be 0 if everything is correct with keys printed to console for mainnet', () => {
+    assert.strictEqual(exitCode, 0)
+  })
+})
+
+// get keys from address given and print json
+describe('get-keys #8', () => {
+  const args = [
+    'get-keys',
+    '-a',
+    'Q020200cf30b98939844cecbaa20e47d16b83aa8de58581ec0fda34d83a42a5a665b49986c4b832',
+    '-i',
+    '1',
+    '-p',
+    '1',
+    '-t',
+    '-j',
+  ]
+  let exitCode
+  before(done => {
+    const process = spawn('./bin/run', args, processFlags)
+    process.on('exit', code => {
+      exitCode = code
+      done()
+    })
+  })
+  it('exit code should be 0 if everything is correct with public keys printed to console in json', () => {
+    assert.strictEqual(exitCode, 0)
+  })
+})
+// get keys from address given and print to file
+describe('get-keys #9', () => {
+  const args = [
+    'get-keys',
+    '-a',
+    'Q020200cf30b98939844cecbaa20e47d16b83aa8de58581ec0fda34d83a42a5a665b49986c4b832',
+    '-i',
+    '2',
+    '-p',
+    '1',
+    '-t',
+    '-f',
+    '/tmp/pub_key_file.json',
+  ]
+  let exitCode
+  before(done => {
+    const process = spawn('./bin/run', args, processFlags)
+    process.on('exit', code => {
+      exitCode = code
+      done()
+    })
+  })
+  it('exit code should be 0 if everything is correct with keys printed to file', () => {
+    assert.strictEqual(exitCode, 0)
+  })
+})
+
+// get keys from address given and print to console without item passed
+describe('get-keys #10', () => {
+  const args = [
+    'get-keys',
+    '-a',
+    'Q020200cf30b98939844cecbaa20e47d16b83aa8de58581ec0fda34d83a42a5a665b49986c4b832',
+    '-t',
+    '-j',
+    '-p',
+    '1',
+  ]
+  let exitCode
+  before(done => {
+    const process = spawn('./bin/run', args, processFlags)
+    process.on('exit', code => {
+      exitCode = code
+      done()
+    })
+  })
+  it('exit code should be 0 if everything is correct with default number of keys printed to console', () => {
+    assert.strictEqual(exitCode, 0)
+  })
+})
+
+// get keys from address given and print to console without item passed
+describe('get-keys #11', () => {
+  const args = [
+    'get-keys',
+    '-a',
+    'Q020200cf30b98939844cecbaa20e47d16b83aa8de58581ec0fda34d83a42a5a665b49986c4b832',
+    '-t',
+    '-j',
+    '-i',
+    '1',
+  ]
+  let exitCode
+  before(done => {
+    const process = spawn('./bin/run', args, processFlags)
+    process.on('exit', code => {
+      exitCode = code
+      done()
+    })
+  })
+  it('exit code should be 0 if everything is correct with default number of keys printed to console', () => {
+    assert.strictEqual(exitCode, 0)
+  })
+})
+// get keys from address given and print to console  No keys found
+describe('get-keys #12', () => {
+  const args = [
+    'get-keys',
+    '-a',
+    'Q000500215d6a512b193aa19f7812bb708251f94e48e176e00bfea0760fa48419feae6ce3ab1637',
+    '-t',
+  ]
+  let exitCode
+  before(done => {
+    const process = spawn('./bin/run', args, processFlags)
+    process.on('exit', code => {
+      exitCode = code
+      done()
+    })
+  })
+  it('exit code should be 0 if everything is correct with no keys found', () => {
+    assert.strictEqual(exitCode, 0)
+  })
+})
+
